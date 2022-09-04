b0906efd9ea4053a2d4692e7a3be98cfe5ee3b6f
behrami <behram1993@gmail.com>
2022-09-04 19:16:55 -0400

    added db tables and migrations for leavetype and leaveallocation

--- LeaveManagment.Web/Data/ApplicationDbContext.cs
+++ LeaveManagment.Web/Data/ApplicationDbContext.cs
@@ -9,5 +9,8 @@ namespace LeaveManagment.Web.Data
             : base(options)
         {
         }
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
     }
 }
