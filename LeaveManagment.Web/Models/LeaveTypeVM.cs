64cc314933adf5672c4e5f849017bfd6f02e08dc
behrami <behram1993@gmail.com>
2022-09-05 21:18:47 -0400

    added CRUD pages for LeaveTypes, also added automapper and added the use of view model

---
+++ LeaveManagment.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagment.Web.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [Display(Name="Default Number of Days")]
+        public int DefaultDays { get; set; }
+    }
+}
